f76381322c5a2b172b99db7ba6115e37c2eee42b
MS10 HPChen0 <HPChen0@nuvoton.com>
2022-12-16 09:44:31 +0800

    Add a C++ project
    1. Add Startup_GCC_C++.S
    2. Add GCC_c++.ld
    3. Add hello_c++ sample
    
    Change-Id: Ifd05eb73680864556c8bc4616d7648a21af6f1ce

---
+++ SampleCode/hello_c++/main.cpp
@@ -0,0 +1,48 @@
+/* Standard includes. */
+#include <stdlib.h>
+
+/* Nuvoton includes. */
+#include "N9H30.h"
+#include "sys.h"
+
+class MyTestClass
+{
+public:
+	MyTestClass():
+		test_variable(2)
+	{
+
+	}
+	int GetTestVariable()
+	{
+		return test_variable;
+	}
+protected:
+	int test_variable;
+};
+
+static MyTestClass my_test_class_a;
+
+int main( void )
+{
+
+	/* Setup the hardware */
+    sysDisableCache();
+    sysFlushCache(I_D_CACHE);
+    sysEnableCache(CACHE_WRITE_BACK);
+    sysInitializeUART();
+    sysSetLocalInterrupt(ENABLE_IRQ);
+
+    sysprintf("\n\nHello World!\r\n");
+
+    int val_a = my_test_class_a.GetTestVariable();
+    sysprintf("static construction: %d\r\n", val_a);
+
+    MyTestClass* my_test_class_b = new MyTestClass();
+    int val_b = my_test_class_b->GetTestVariable();
+    sysprintf("dynamic construction: %d\r\n", val_b);
+    delete my_test_class_b;
+
+    return 0;
+}
+
